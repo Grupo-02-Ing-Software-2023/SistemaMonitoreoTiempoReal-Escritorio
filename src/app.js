62ad1bf6cd75a73a9f82ac69123de21f8bd8d07b
Marlon Salomon Coreas Villanueva <39624475+CoreasMarlon@users.noreply.github.com>
2023-11-05 22:44:28 -0600

    Sistema de Monitoreo Complemento

--- src/app.js
+++ src/app.js
@@ -1,27 +1,54 @@
 const { ipcRenderer } = require("electron")
 
-window.addEventListener("DOMContentLoaded", () => {
+const check = document.getElementById('flexSwitchCheckDefault')
+const inputSala = document.getElementById('inputSala')
 
-    /*
-    document.getElementById('btnScreenshot').addEventListener("click", () => {
-        ipcRenderer.send("screenshot:capture", {})
-    })*/
+ipcRenderer.on("screenshot:capture", (e, value) => {
+    if (value.status) {
 
-    ipcRenderer.on("screenshot:capture", (e, imageData) => {
-        document.getElementById("placeholder").src = imageData
-    })
-})
+        const date = new Date(value.inicioSesion);
 
-document.getElementById('flexSwitchCheckDefault').addEventListener("change", (event) => {
-    const isChecked = event.target.checked
-    ipcRenderer.send("screenshot:capture", { isChecked })
-
-
-    /*
-    if (event.target.checked) {
-        ipcRenderer.send("screenshot:capture", {})
+        document.getElementById('mensaje1').textContent = "Conectado";
+        document.getElementById('card1').textContent = "Sala: " + value.nombreSala;
+        document.getElementById('card2').textContent = formateHour(date);
     } else {
-        alert('El checkbox ha sido desactivado');
+        alert(value.msg || "Error al conectarse a la sala");
+        document.getElementById('flexSwitchCheckDefault').checked = false;
+        //document.getElementById('inputSala').disabled = false;
+        //document.getElementById('inputSala').readOnly = true;
+
     }
-    */
 })
+
+check.addEventListener("change", (event) => {
+    const codigoSala = inputSala.value;
+
+    if (codigoSala.trim().length == 0) {
+        alert("Ingresa el codigo de la sala");
+        desconecxion();
+    } else {
+        const isChecked = event.target.checked;
+        if(!isChecked){
+            desconecxion()
+        }
+        inputSala.disabled = isChecked;
+        ipcRenderer.send("screenshot:capture", { codigoSala, isChecked })
+    }
+})
+
+function formateHour(date) {
+    const horas = String(date.getHours()).padStart(2, '0');
+    const minutos = String(date.getMinutes()).padStart(2, '0');
+    const segundos = String(date.getSeconds()).padStart(2, '0');
+
+    // Formatea la hora en un formato legible
+    return `Hora de conexión: ${horas}:${minutos}:${segundos}`;
+}
+
+function desconecxion() {
+    document.getElementById('flexSwitchCheckDefault').checked = false;
+    document.getElementById('inputSala').disabled = false;
+    document.getElementById('mensaje1').textContent = "Conectate a una sala para iniciar";
+    document.getElementById('card1').textContent = "No estas conectado a ninguna sala";
+    document.getElementById('card2').textContent = "No estas conectado a ninguna sala";
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-const { app, BrowserWindow, desktopCapturer, ipcMain, screen } = require("electron");
+const { app, BrowserWindow, desktopCapturer, ipcMain, screen, dialog } = require("electron");
 const Store = require('electron-store');
 const axios = require('axios');
+const FormData = require('form-data');
 let fs = require('fs');
 let path = require("path");
+const { config } = require("dotenv");
+
+require('dotenv').config();
+const baseURL = process.env.baseURL || "https://igf-backend-production.up.railway.app/api";
 
 let window = null;
 let timerId = null;
@@ -40,24 +45,25 @@ function WriteFile(image) {
 }
 
 app.on('window-all-closed', () => {
+    store.delete('token');
+    store.delete('usuario');
+    store.delete('sesion');
+    store.delete('sala');
     if (process.platform !== 'darwin') app.quit()
 })
 
 
 ipcMain.on("login", async (e, { email, password }) => {
     try {
-        const response = await axios.post('https://igf-backend-production.up.railway.app/api/auth/login', { email, password });
+        const response = await axios.post(baseURL + '/auth/login', { email, password });
         const data = response.data;
 
         if (response.status === 200) {
             if (data.usuario && data.token) {
-                
+
                 store.set('usuario', data.usuario);
                 store.set('token', data.token);
-                //console.log(store.get('usuario'));
-                //console.log(store.get('token'));
                 window.loadFile(path.join("src/ui/index.html"))
-                //createWindowIndex()
             } else {
                 let msg = 'Respuesta de inicio de sesión no válida.'
                 window.webContents.send("login", msg)
@@ -73,10 +79,67 @@ ipcMain.on("login", async (e, { email, password }) => {
     }
 })
 
+ipcMain.on("screenshot:capture", (e, value) => {
+    if (value.isChecked) {
+        conectarSala(value.codigoSala);
+    } else {
+        clearInterval(timerId);
+        const hora_fin_sesion = new Date();
+        const config = {
+            headers: {
+                'x-token': store.get('token'),
+                'Content-Type': 'application/json',
+                'Accept-Charset': 'utf-8'
+            }
+        };
+        axios.put(`${baseURL}/sesiones/${store.get('sesion')._id}`, { hora_fin_sesion }, config)
+        .then(response => {
+            const data = response.data;
+            store.delete('sesion');
+            store.delete('sala');
+        })
+        .catch(error => {
+            console.log(error)
+        });
+    }
+})
 
-const createWindowIndex = () => {
-    window.loadFile(path.join("src/ui/index.html"))
 
+const conectarSala = async (codigoSala) => {
+    const config = {
+        headers: {
+            'x-token': store.get('token'),
+            'Content-Type': 'application/json',
+            'Accept-Charset': 'utf-8'
+        }
+    };
+
+    axios.get(`${baseURL}/salas/${codigoSala}`, config)
+        .then(response => {
+            const data = response.data;
+            store.set('sala', data);
+
+            const hora_inicio_sesion = fecha_sesion = new Date();
+            const hora_fin_sesion = "";
+            const sala = data._id;
+
+            return axios.post(baseURL + '/sesiones/', {hora_inicio_sesion, hora_fin_sesion, fecha_sesion, sala }, config)
+            
+        })
+        .then(secondResponse => {
+            const secondData = secondResponse.data;
+            store.set('sesion', secondData.sesion);
+            console.log(secondData.sesion)
+
+            window.webContents.send("screenshot:capture", { status: true, inicioSesion: secondData.sesion.hora_inicio_sesion, nombreSala: store.get('sala').nom_sala })
+            CrearCaptura();
+        })
+        .catch(error => {
+            window.webContents.send("screenshot:capture", { status: false, msg: error.message })
+        });
+}
+
+const CrearCaptura = () => {
     timerId = setInterval(function () {
         const mainScreen = screen.getPrimaryDisplay();
         desktopCapturer
@@ -89,43 +152,99 @@ const createWindowIndex = () => {
             })
             .then((sources) => {
                 let image = sources[0].thumbnail.toDataURL()
-                window.webContents.send("screenshot:capture", image)
-                WriteFile(image)
+                EnviarCaptura(image)
             })
             .catch(err => {
                 console.log(err)
             })
-    }, 5000);
-
-    window.on('closed', function () {
-        clearInterval(timerId);
-        window = null;
-    });
+    }, process.env.TIME || 20000);
 }
 
-ipcMain.on("screenshot:capture", (e, value) => {
-    if (value.isChecked) {
-        timerId = setInterval(function () {
-            const mainScreen = screen.getPrimaryDisplay();
-            desktopCapturer
-                .getSources({
-                    types: ["screen"],
-                    thumbnailSize: {
-                        width: mainScreen.size.width,
-                        height: mainScreen.size.height
-                    }
-                })
-                .then((sources) => {
-                    let image = sources[0].thumbnail.toDataURL()
-                    window.webContents.send("screenshot:capture", image)
-                    WriteFile(image)
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-        }, 5000);
-    } else {
-        clearInterval(timerId);
-    }
-})
+const EnviarCaptura = (image) => {
 
+    const url_captura = "";
+    const fecha_captura = new Date();
+    const sesion = store.get('sesion')._id;
+    const usuario = store.get('usuario');
+    const nom_captura = nombreCaptura(usuario._id);
+    const savePath = `${__dirname}/user_images/${nom_captura}.jpg`;
+    createImageFileFromBase64(image, savePath);
+
+    const config = {
+        headers: {
+            'x-token': store.get('token'),
+            'Content-Type': 'application/json',
+            'Accept-Charset': 'utf-8'
+        }
+    };
+
+    axios.post(baseURL + '/capturas/', { url_captura, nom_captura, fecha_captura, sesion, usuario }, config)
+        .then(response => {
+            const data = response.data;
+            console.log(data)
+
+            const form = new FormData();
+            form.append('archivo', fs.createReadStream(savePath));
+
+            const config2 = {
+                headers: {
+                    'x-token': store.get('token'),
+                    ...form.getHeaders(),
+                    'Accept-Charset': 'utf-8'
+                }
+            };
+
+            return axios.put(baseURL + '/uploadCaptura/' + data.captura._id, form, config2)
+        })
+        .then(secondResponse => {
+            const secondData = secondResponse.data;
+            console.log(secondData);
+
+            // Verifica si el archivo existe antes de intentar eliminarlo
+            if (fs.existsSync(savePath)) {
+                fs.unlink(savePath, (err) => {
+                    if (err) {
+                        console.error('Error al eliminar el archivo:', err);
+                    } else {
+                        console.log('Archivo eliminado con exito.');
+                    }
+                });
+            }
+        })
+        .catch(error => {
+            if (error.response) {
+                console.log(error.response.data.msg);
+            } else {
+                console.error(error.message);
+            }
+        });
+}
+
+async function createImageFileFromBase64(base64String, savePath) {
+
+    // Convierte la cadena Base64 en un blob
+    const byteCharacters = atob(base64String.split(',')[1]);
+    const byteNumbers = new Uint8Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const blob = new Blob([byteNumbers], { type: 'image/jpg' });
+
+    // Guarda el archivo en la ruta de destino
+    const buffer = Buffer.from(await blob.arrayBuffer());
+    fs.writeFileSync(savePath, buffer);
+}
+
+function nombreCaptura(usuario_id) {
+    const date = new Date();
+    const año = date.getFullYear();
+    const mes = String(date.getMonth() + 1).padStart(2, '0'); // Agrega un 0 al principio si es necesario
+    const dia = String(date.getDate()).padStart(2, '0');
+    const horas = String(date.getHours()).padStart(2, '0');
+    const minutos = String(date.getMinutes()).padStart(2, '0');
+    const segundos = String(date.getSeconds()).padStart(2, '0');
+
+    // Formatea la fecha para formar el nombre de la imagen
+    const nombreImagen = `${año}-${mes}-${dia}_${horas}-${minutos}-${segundos}_${usuario_id}`;
+    return nombreImagen;
+}
